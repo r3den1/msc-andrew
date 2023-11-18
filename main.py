86ba3e2933dc4fe8bf4115531383a425deb1e447
Andrew <smislmemi@gmail.com>
2023-11-18 22:21:00 +0300

    circles v 1.1

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
 import sys
 from PyQt5 import QtCore
-from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
 from PyQt5.QtGui import QPainter, QColor, QBrush
 
@@ -10,10 +9,10 @@ class RandomCircles(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(0, 0, 500, 500)
-        uic.loadUi('Ui.ui', self)
+        self.pushButton = QPushButton(self)
+        self.pushButton.setText('Click me!!!')
         self.pushButton.clicked.connect(self.paint)
         self.do = False
-        self.color = QColor(0, 255, 255)
 
     def paintEvent(self, event):
         if self.do:
@@ -27,7 +26,10 @@ class RandomCircles(QMainWindow):
         self.repaint()
 
     def draw_circle(self, qp):
-        qp.setBrush(QColor(QtCore.Qt.yellow))
+        qp.setBrush(QColor(random.randrange(0, 256),
+                           random.randrange(0, 256),
+                           random.randrange(0, 256)
+                           ))
         x = random.randrange(0, 500)
         y = random.randrange(0, 500)
         r = random.randrange(0, 250)
